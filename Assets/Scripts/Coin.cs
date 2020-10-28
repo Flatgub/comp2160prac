d152ad04240da21481181a1214be9157a2424fa0
Chloe.H <chloecute0@gmail.com>
2020-10-29 09:53:30 +1100

    coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //not exactly optimial, but it works for the sake of the prac
+        FindObjectOfType<ScoreKeeper>().OnPickup();
+        Destroy(gameObject);
+    }
+}
