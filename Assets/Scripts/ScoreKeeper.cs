a31a2debe0a34b50563520097e44fc35aeed1089
Chloe.H <chloecute0@gmail.com>
2020-10-29 09:14:55 +1100

    added score keeper

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+
+    [SerializeField]
+    private int pointsPerPickup = 1;
+    public int Score 
+    {
+        get;
+        private set;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    public void OnPickup()
+    {
+        Score += pointsPerPickup;
+    }
+}
